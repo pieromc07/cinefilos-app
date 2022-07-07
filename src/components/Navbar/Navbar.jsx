e000635976f22d22c63b122ccd0f59521e057229
PieroMC <piero0716.mc@gmail.com>
2022-07-07 16:00:21 -0500

    :lipstick: navbar redesign

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,74 +1,107 @@
 import React, { useState } from 'react'
-import { FaBars } from 'react-icons/fa'
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
 import logo from '../../logo.svg'
 import { BaseInput } from '../../shared/components/BaseInput/BaseInput'
-import { Button } from '../../shared/components/Button/Button'
 import './Navbar.css'
 
 
 
 export const Navbar = () => {
-    const [toggler, setToggler] = useState(false)
-    const handleToggler = () => {
-        setToggler(!toggler)
 
-        if (toggler) {
-            document.querySelector('#navbarMenu').classList.remove('show')
-        } else {
-            document.querySelector('#navbarMenu').classList.add('show')
-        }
+    const [isOpen, setIsOpen] = useState(false)
 
+    const handleOpen = () => {
+        setIsOpen(!isOpen)
+
+        isOpen ?
+            document.getElementById('collapse').classList.add('closed')
+            :
+            document.getElementById('collapse').classList.remove('closed')
     }
 
-
     return (
-        <div className="navbar">
-            <div className="navbar-logo">
-                <img src={logo} alt="logo" />
-                <div className="navbar-brand">
-                    <strong>Cinefilos</strong>
-                    <div className="brand_bar"></div>
+        <>
+            <div className="navbar">
+                <div className="navbar-logo">
+                    <img src={logo} alt="logo" />
+                    <div className="navbar-brand">
+                        <strong>Cinefilos</strong>
+                        <div className="brand_bar"></div>
+                    </div>
                 </div>
-            </div>
-            <button className="navbar-toggler" onClick={handleToggler}>
-                <FaBars />
-            </button>
-            <div className="navbar-menu" id='navbarMenu'>
-                <h3 className='menu_title'>Detalles del Cine</h3>
-                <BaseInput name={'Ubicacion'}
-                    options={[
-                        { option: 'Trujillo', index: 1 },
-                        { option: 'Lima', index: 2 },
-                        { option: 'Arequipa', index: 3 },
-                        { option: 'Cusco', index: 4 },
-                        { option: 'Chiclayo', index: 5 },
-                        { option: 'Tacna', index: 6 },
-                        { option: 'Iquitos', index: 7 }
-                    ]}
-                />
-                <BaseInput name={'Cines Cercanos'}
-                    options={[
-                        { option: 'Cineplanet', index: 1 },
-                        { option: 'Cinemark', index: 2 },
-                        { option: 'Cinepolis', index: 3 }
-                    ]}
-                />
-                <BaseInput name={'Fecha'}
-                    options={[
-                        { option: '7 de Julio', index: 1 },
-                        { option: '14 de Julio', index: 2 },
-                        { option: '21 de Julio', index: 3 },
-                        { option: '28 de Julio', index: 4 },
-                        { option: '5 de Agosto', index: 5 }
-                    ]}
-                />
-                <div className="menu_button">
-                    <Button text={'Filtar'} status={true} bg="light"
+                <div className="navbar-menu" id='navbarMenu'>
+                    <BaseInput name={'Ubicacion'}
+                        options={[
+                            { option: 'Trujillo', index: 1 },
+                            { option: 'Lima', index: 2 },
+                            { option: 'Arequipa', index: 3 },
+                            { option: 'Cusco', index: 4 },
+                            { option: 'Chiclayo', index: 5 },
+                            { option: 'Tacna', index: 6 },
+                            { option: 'Iquitos', index: 7 }
+                        ]}
+                    />
+                    <BaseInput name={'Cines Cercanos'}
+                        options={[
+                            { option: 'Cineplanet', index: 1 },
+                            { option: 'Cinemark', index: 2 },
+                            { option: 'Cinepolis', index: 3 }
+                        ]}
+                    />
+                    <BaseInput name={'Fecha'}
+                        options={[
+                            { option: '7 de Julio', index: 1 },
+                            { option: '14 de Julio', index: 2 },
+                            { option: '21 de Julio', index: 3 },
+                            { option: '28 de Julio', index: 4 },
+                            { option: '5 de Agosto', index: 5 }
+                        ]}
                     />
                 </div>
             </div>
 
+            <div className="navbar-menu" id='navbarMenuMobile'>
+                <div className="menu-title">
+                    <h3 className="title_text">Detalles del Cine</h3>
 
-        </div>
+                    <button className="title_icon" onClick={handleOpen}>
+                        {
+                            isOpen ? <FaAngleUp /> : <FaAngleDown />
+                        }
+                    </button>
+
+                </div>
+                <div className="menu-content closed" id='collapse'>
+                    <BaseInput name={'Ubicacion'}
+                        options={[
+                            { option: 'Trujillo', index: 1 },
+                            { option: 'Lima', index: 2 },
+                            { option: 'Arequipa', index: 3 },
+                            { option: 'Cusco', index: 4 },
+                            { option: 'Chiclayo', index: 5 },
+                            { option: 'Tacna', index: 6 },
+                            { option: 'Iquitos', index: 7 }
+                        ]}
+                    />
+                    <BaseInput name={'Cines Cercanos'}
+                        options={[
+                            { option: 'Cineplanet', index: 1 },
+                            { option: 'Cinemark', index: 2 },
+                            { option: 'Cinepolis', index: 3 }
+                        ]}
+                    />
+                    <BaseInput name={'Fecha'}
+                        options={[
+                            { option: '7 de Julio', index: 1 },
+                            { option: '14 de Julio', index: 2 },
+                            { option: '21 de Julio', index: 3 },
+                            { option: '28 de Julio', index: 4 },
+                            { option: '5 de Agosto', index: 5 }
+                        ]}
+                    />
+                </div>
+            </div>
+        </>
+
     )
 }
