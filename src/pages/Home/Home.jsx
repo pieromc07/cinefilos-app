622da77c0f334e96ada4792d54655356bc2aa8e1
PieroMC <piero0716.mc@gmail.com>
2022-07-07 16:01:36 -0500

    :art: structure and design the of Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Navbar } from '../../components/Navbar/Navbar'
 
+import { movies } from '../../data/Movies'
+import { Poster } from '../../shared/components/Poster/Poster'
+
+import './Home.css'
+
+
 const Home = () => {
-  return (
-    <div>
-        <Navbar/>
-        <h1>Hola mundo</h1>
-    </div>
-  )
+	return (
+		<>
+			<Navbar />
+			<div className="home-container">
+				<div className="home-title">
+					<h1 className="title">Cartelera</h1>
+					<p className="subtitle">Estas son todas las peliculas disponibles.</p>
+				</div>
+
+				<div className="home-content">
+					{movies.map(movie => (
+						<div className="movie-card" key={movie.id}>
+							<Poster title={movie.title} image={movie.image} />
+						</div>
+					))}
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Home
---
+++ src/shared/components/Poster/Poster.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Poster.css'
+
+export const Poster = ({title, image}) => {
+    return (
+
+        <img
+            className="movie"
+            src={image} 
+            alt={title} 
+        />
+    )
+}
