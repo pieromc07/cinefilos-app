110c4d3294e1a25e4ed1e1b2fdf9a9048390a1a0
Piero Merino <piero0716.mc@gmail.com>
2022-08-11 23:11:07 -0500

    add data test

---
+++ src/data/Movies.js
@@ -0,0 +1,49 @@
+export const movies = [
+
+    {
+        id: 1,
+        title: 'Bestia',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001398'
+    },
+    {
+        id: 2,
+        title: 'Dino King: Viaje a la Montaña de Fuego',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00000953'
+    },
+    {
+        id: 3,
+        title: 'El hijo del diablo',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001313'
+    },
+    {
+        id: 4,
+        title: 'Festival Nick JR.',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001383'
+    },
+    {
+        id: 5,
+        title: 'Soldado virtual',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001432'
+    },
+    {
+        id: 6,
+        title: 'Buena Suerte Leo Grande',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001382'
+    },
+    {
+        id: 7,
+        title: 'Cosas de Amigos',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001272'
+    },
+    {
+        id: 8,
+        title: 'Thor: Amor Y Trueno',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001344'
+    },
+    {
+        id: 9,
+        title: 'Top Gun Maverick',
+        image: 'https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00000983'
+    }
+]
+
