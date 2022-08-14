c39bacbaebff29d7a76a17d8d6cd2cd0563710eb
Piero mc <piero0716.mc@gmail.com>
2022-08-14 07:28:53 -0500

    :heavy_plus_sign: Components: ButtonIcon created

---
+++ src/shared/components/ButtonIcon/ButtonIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './ButtonIcon.css'
+
+export const ButtonIcon = ({text, icon}) => {
+  return (
+    <button className='button'>
+        <img className='icon' src={icon} alt={`icon ${text}`} />
+        <span>{text}</span>
+    </button>
+  )
+}
